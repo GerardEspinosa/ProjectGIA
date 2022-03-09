b837f861c53dd6625498f6e165a5b0492e067030
Adrià Rivero <adriariverosegarra@gmail.com>
2022-03-09 17:21:49 +0100

    Login corregido y remember me añadido

---
+++ GIA/js/main.js
@@ -0,0 +1,25 @@
+rmCheck = document.getElementById("checkRemember");
+user = document.getElementById("user");
+
+
+//Al entrar a la pantalla de login comprobamos en el localStorage si hay que activar el checknox y rellenar el campo de usuario
+if (localStorage.checkbox && localStorage.checkbox !== "") {
+    rmCheck.setAttribute("checked", "checked");
+    user.value = localStorage.username;
+} else {
+    rmCheck.removeAttribute("checked");
+    user.value = "";
+}
+
+function lsRememberMe() {
+    // Si el check está marcado y el usuario no está vacio guardamos los valores en el localStorage
+    if (rmCheck.checked && user.value !== "") {
+        localStorage.username = user.value;
+        localStorage.checkbox = rmCheck.value;
+    }
+    //Si el check está desmarcado vaciamos los valores en el localStorage
+    else {
+        localStorage.username = "";
+        localStorage.checkbox = "";
+    }
+}
