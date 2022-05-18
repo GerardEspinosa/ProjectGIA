b9b7cda27a05dd16d78769394563ab0b4f26e47e
Gerard Espinosa Yxart <gerard.espinosa.yxart@gmail.com>
2022-05-18 16:57:01 +0200

    corrección

--- GIA/php/login.php
+++ GIA/php/login.php
@@ -24,7 +24,7 @@
             $result2 = mysqli_query($connection, $query2);
             if(mysqli_num_rows($result)==1 || mysqli_num_rows($result2)==1)
             {
-                header("LOCATION: ../index.html");
+                header("LOCATION: ../index.php");
             }
             else
             {
@@ -35,7 +35,7 @@
                 if(mysqli_num_rows($result)==1 || mysqli_num_rows($result2)==1)
                 {
                     $row = mysqli_fetch_assoc($result);
-                    $row = mysqli_fetch_assoc($result2);
+                    $row2 = mysqli_fetch_assoc($result2);
                     if ($row["Cambiar_Contraseña"] == TRUE || $row2["CambiarContraseña"] == TRUE){
                         $id = $row["id_Empleado"];
                         $empleadoUsuario = "'" . $row["Usuario_Empleado"] . "'";
@@ -46,7 +46,7 @@
                         }
                     }
                     else{
-                        header("LOCATION: ../index.html");
+                        header("LOCATION: ../index.php");
                     }
                 }
                 else{
